21b473b58c8b867558644f5107f08db5e584feba
Daniel Selga <danielselga@gmail.com>
2021-10-02 14:35:48 -0300

    chore: ES6 vs Script

--- script.js
+++ script.js
@@ -9,3 +9,12 @@
 // Isolate Components: Modules can be developed in isolation without thinking about the entire code base.
 // Abstract code: Implement low level code in modules and import these abstractions into other modules.
 // Reuse code: Modules allow us to easily reuse the same code, even across multiple projects.
+
+// ES6 Modules -> Modules are stored in files, exatly one module per file.
+
+// ES6 Mules VS Scripts
+// TOP-LEVEL VARIABLES: Scoped to module - Global
+// Default mode: 'strict-mode' - "Sloppy mode"
+// Top level this: 'Undefined' - Window
+// HTML Linking: '<stript type="module">' - <script>
+// File downloading: Asynchronous - Synchronous
