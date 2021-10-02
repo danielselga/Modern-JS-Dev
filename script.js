061f60ed37ae168b7033bc69b1406ca558a9c595
Daniel Selga <danielselga@gmail.com>
2021-10-02 14:30:31 -0300

    chore: start project

---
+++ script.js
@@ -0,0 +1,11 @@
+// Modules -> are reusable piece of code that encapsulate implementations details.
+
+// Usualy a standalone file, but it doesent have to be.
+
+
+// IMPORT -> Dependencies, Export -> (Public api)
+
+// Compose softwares: Modules are small building blocks that we put all together to build complex applications.
+// Isolate Components: Modules can be developed in isolation without thinking about the entire code base.
+// Abstract code: Implement low level code in modules and import these abstractions into other modules.
+// Reuse code: Modules allow us to easily reuse the same code, even across multiple projects.
